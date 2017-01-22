53e53f4bed3e91a885861d64e6faae92521c064b
warrenchan <w.warrenchan@gmail.com>
2017-01-22 03:10:25 -0800

    Slider installed//Category font fixed// gradient added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+$(function(){
+
+    $('.subbutt').on('click', function(){
+        var emailInput = ('email').val;
+        
+        if ('emailInput' == "") {
+            alert('Empty email slot..');
+        }
+        // console.log('hope');
+    //     if 
+    //    prompt('Thank you for your email.');
+        
+    });
+
+
+    $('.main-carousel').flickity({
+        cellAlign: 'center',
+        contain: true,
+        freescroll: true,
+        groupCells: 1,
+        prevNextButtons: false,
+        pageDots: true,
+        resize: true,
+        autoPlay: true,
+        });
+
+    $('')({
+
+    })
+
+
+});
 
