fcca48024866dc5a193ebfbc536789afbe0422fa
warrenchan <w.warrenchan@gmail.com>
2017-01-23 04:27:59 -0800

    fixed navbar/implimented smoothscroll/fixed carousel/changed grid hover colour

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 $(function(){
-
-
+        // smooth scrolling
         $('a[href^="#"]').on('click', function (event) {
-            event.preventDefault();
+        event.preventDefault();
 
-            var target = this.hash,       // takes '#' of 'a href'
-                $target = $(target);      // targets '#' of 'a href'
+        var target = this.hash, 
+        // takes # of 'a href'
+            $target = $(target); 
+            // targets # of 'a href'
 
-            $('html, body').animate({                 // animate method on the body
-                scrollTop: $target.offset().top - 90   // scrolltop animate method smooth scroll to corresponding 'a href' #
-            }, 900)                                                               // .top 116 for fixed nav bar. 900 for scroll speed in millisec
-        });
+        $('html, body').animate({ 
+            // animate method on the body
+            'scrollTop': $target.offset().top - 116
+            // scrolltop animate method smooth scroll to corresponding 'a href' #
+        }, 400) 
+        // .top 116 for fixed nav bar. 900 for scroll speed in millisec
+    });
 
 
     $('.subbutt').on('click', function(event){
@@ -20,7 +24,7 @@ $(function(){
 
 
     $('.main-carousel').flickity({
-        cellAlign: 'center',
+        cellAlign: 'left',
         contain: true,
         freescroll: true,
         groupCells: 1,
@@ -28,6 +32,7 @@ $(function(){
         pageDots: true,
         resize: true,
         autoPlay: true,
+
         });
 
 });
