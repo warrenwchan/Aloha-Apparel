badd3381a053abfcca63a6df7fca4c31ea6ed5ae
Warren Chan <warrenchan@Warrens-MacBook-Pro.local>
2017-03-10 17:12:39 -0800

    code clean up, html code indentiation

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
-$(function(){
-        // smooth scrolling
-        $('a[href^="#"]').on('click', function (event) {
+$(function () {
+    // smooth scrolling
+    $('a[href^="#"]').on('click', function (event) {
         event.preventDefault();
 
-        var target = this.hash, 
-        // takes # of 'a href'
-            $target = $(target); 
-            // targets # of 'a href'
+        var target = this.hash,
+            // takes # of 'a href'
+            $target = $(target);
+        // targets # of 'a href'
 
-        $('html, body').animate({ 
+        $('html, body').animate({
             // animate method on the body
             'scrollTop': $target.offset().top - 116
             // scrolltop animate method smooth scroll to corresponding 'a href' #
-        }, 400) 
+        }, 400)
         // .top 116 for fixed nav bar. 900 for scroll speed in millisec
     });
 
 
-    $('.subbutt').on('click', function(event){
-       event.preventDefault();
-       confirm('Thanks for marking this');
+
+    $('.subbutt').on('click', function (event) {
+        event.preventDefault();
+        const isItValid = formValidation($('.email').val())
+        
+        if(isItValid){
+            confirm('Thanks for marking this');
+        } else {
+            confirm('Please stop being dumb')
+        }
     });
 
 
@@ -33,7 +40,9 @@ $(function(){
         resize: true,
         autoPlay: true,
 
-        });
+    });
 
+    function formValidation(string) {
+        return /.+@.+\..+/gi.test(string)
+    }
 });
-
